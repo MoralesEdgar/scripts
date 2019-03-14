9f9f8ecf227e414d60e8ba21dd58bc6dfc65d0b3
edgarmorales <edgar.morales@tecmor.mx>
2019-03-14 17:33:17 -0600

    Fix - Se corrige el script principal.

--- script_bash.sh
+++ script_bash.sh
@@ -19,6 +19,7 @@ echo " 4.Añadir un usuario a un grupo"
 echo " 5.Ver datos de un usuario"
 echo " 6.Borrar un usuario"
 echo " 7.Borrar un grupo"
+echo " 8.Ver los grupos"
 echo " 0.Salir"
 
 read opcion
@@ -60,16 +61,23 @@ echo "Ver datos de un usuario"
 echo "Inserta el nombre del usuario"
 read user5
 id $user5
+;;
 
 6)
 echo "Dame el nombre del usuario"
 read nombre
 deluser $nombre
+;;
 
 7)
 echo"Dame el nombre del grupo: "
 read grupo
 delgroup $grupo
+;;
+
+8)
+echo"Los grupos son los siguientes: "
+vim /etc/group
 ;;
 
 esac
