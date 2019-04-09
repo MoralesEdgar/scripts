b5aae58b1d225069c0e2359a2dfb97a07a02d57c
edgarmorales <edgar.morales@tecmor.mx>
2019-04-09 00:52:22 -0500

    Add - Se agregan comandos

--- script1.sh
+++ script1.sh
@@ -5,14 +5,18 @@ read nombre_usuario
 mkdir $nombre_usuario
 echo "Cuantas subcarpetas quieres crear?"
 read num_folder
-for item in $num_folder; do
-  mkdir -p $nombre_usuario/folder_$item
+for ((i=1; i<=$num_folder; i++))do
+  mkdir -p $nombre_usuario/folder_$i
 done
-echo "Deseas crear un achivo?
+echo "Deseas crear un achivo?"
 read respuesta_archivo
-if[$respuesta_archivo];then
+if [ $respuesta_archivo != '' ];
+then
 	echo "Escribe el nombre del archivo:"
 	read name_file
-	$nombre_usuario/touch $name_file
-tree -L X
-
+	cd $nombre_usuario
+	touch $name_file
+	tree -L X
+	exit 0
+fi
+exit0	
